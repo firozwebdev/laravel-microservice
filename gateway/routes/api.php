0b7aab5c5668a36e9952a8fd52ef71c85c10c8fb
Sabuz <everythingkst@gmail.com>
2025-05-29 21:40:17 +0600

    working on microservices

--- gateway/routes/api.php
+++ gateway/routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// $response = Http::get('http://product-service:8000/api/products');
+// return $response->json();
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/products', function () {
+    $response = Http::get('http://product-service:8000/api/products');
+    return $response->json();
 });
---
+++ product-service/app/Http/Controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Product::all());
+    }
+
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+        ]);
+
+        $product = Product::create($data);
+        return response()->json($product, 201);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $data = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'sometimes|numeric',
+            'stock' => 'sometimes|integer',
+        ]);
+
+        $product->update($data);
+        return response()->json($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(['message' => 'Product deleted']);
+    }
+}
