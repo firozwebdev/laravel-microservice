7fb86a37b36f4db6dc8366059d0a6683883d1108
Rambo <everythingkst@gmail.com>
2025-05-31 20:41:46 +0600

    route added to web.php in product-service

--- product-service/routes/web.php
+++ product-service/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Product;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-   return "product service is running";
+   return Product::all();
 });
